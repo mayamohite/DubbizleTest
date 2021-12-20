52d8ec0075d434617bf5a031b0160c609a2f3c99
mayamohite <mayapmohite@gmail.com>
2021-12-20 15:43:55 +0530

    Replace Result error type with string

--- app/src/main/java/com/example/dubizzletest/domain/common/Result.kt
+++ app/src/main/java/com/example/dubizzletest/domain/common/Result.kt
@@ -7,13 +7,13 @@ package com.example.dubizzletest.domain.common
 sealed class Result<out R> {
 
     data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
+    data class Error(val error: String) : Result<Nothing>()
     object Loading : Result<Nothing>()
 
     override fun toString(): String {
         return when (this) {
             is Success<*> -> "Success[data=$data]"
-            is Error -> "Error[exception=$exception]"
+            is Error -> "Error[exception=$error]"
             Loading -> "Loading"
         }
     }
