3b3335a7b2fde656c076b8e2f4ba165f31410b2a
mayamohite <mayapmohite@gmail.com>
2021-12-20 11:24:59 +0530

    Include data source instance

--- app/src/main/java/com/example/dubizzletest/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/dubizzletest/data/repository/ProductRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package com.example.dubizzletest.data.repository
 
+import com.example.dubizzletest.domain.ProductDataSource
 import com.example.dubizzletest.domain.ProductRepository
 import com.example.dubizzletest.domain.entities.Product
+import javax.inject.Inject
 
-class ProductRepositoryImpl : ProductRepository {
+class ProductRepositoryImpl @Inject constructor(
+    private val productDataSource: ProductDataSource,
+) : ProductRepository {
 
     override suspend fun getProductList(): List<Product> {
-        return emptyList()
+        return productDataSource.getProductList()
     }
 }
