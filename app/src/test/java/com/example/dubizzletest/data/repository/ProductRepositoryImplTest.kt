422f1aa0f6cce1be9b184b1d28624c2877df577e
mayamohite <mayapmohite@gmail.com>
2021-12-21 19:34:13 +0530

    Add test cases of repository

---
+++ app/src/test/java/com/example/dubizzletest/data/repository/ProductRepositoryImplTest.kt
@@ -0,0 +1,48 @@
+package com.example.dubizzletest.data.repository
+
+import com.example.dubizzletest.data.utils.PRODUCT_ENTITY
+import com.example.dubizzletest.domain.ProductDataSource
+import com.example.dubizzletest.domain.ProductRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import com.example.dubizzletest.domain.common.Result
+
+class ProductRepositoryImplTest {
+
+    lateinit var repository: ProductRepository
+
+    @Mock
+    lateinit var dataSource: ProductDataSource
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        repository = ProductRepositoryImpl(dataSource)
+    }
+
+    @Test
+    fun `test failure result when error get from data source`() = runBlocking {
+        Mockito.`when`(dataSource.getProductList())
+            .thenReturn(Result.Error("Products not available"))
+
+        val repositoryDetails = repository.getProductList()
+
+        assert(repositoryDetails is Result.Error)
+    }
+
+    @Test
+    fun `test success result`() = runBlocking {
+        Mockito.`when`(dataSource.getProductList())
+            .thenReturn(Result.Success(PRODUCT_ENTITY))
+
+        val repositoryDetails = repository.getProductList()
+
+        assert(repositoryDetails is Result.Success)
+        assertEquals(PRODUCT_ENTITY, (repositoryDetails as Result.Success).data)
+    }
+}
