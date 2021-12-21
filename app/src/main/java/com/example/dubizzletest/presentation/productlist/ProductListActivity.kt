2aff9fc761d42e7e194eb2cf1683afd38df94fb3
mayamohite <mayapmohite@gmail.com>
2021-12-21 23:28:50 +0530

    Implement custom progress bar

--- app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductListActivity.kt
+++ app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductListActivity.kt
@@ -1,9 +1,10 @@
-package com.example.dubizzletest
+package com.example.dubizzletest.presentation.productlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dubizzletest.R
 
-class MainActivity : AppCompatActivity() {
+class ProductListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
