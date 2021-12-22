5da5ea7e6b974d2b1eb5a52166f68f744132c577
mayamohite <mayapmohite@gmail.com>
2021-12-22 13:19:26 +0530

    Launch product fragment from activity

--- app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductListActivity.kt
+++ app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductListActivity.kt
@@ -3,14 +3,20 @@ package com.example.dubizzletest.presentation.productlist
 import android.os.Bundle
 import com.example.dubizzletest.R
 import com.example.dubizzletest.base.BaseActivity
+import com.example.dubizzletest.presentation.util.ImageCache
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProductListActivity : BaseActivity() {
 
+    @Inject
+    lateinit var imageCache: ImageCache
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         launchProductListFragment()
+        imageCache.initializeCache()
     }
 
     override fun getLayout(): Int {
