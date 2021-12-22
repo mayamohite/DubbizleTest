b1be4ed2d6f298c23901165b4efe7b803bbbd762
mayamohite <mayapmohite@gmail.com>
2021-12-22 13:18:30 +0530

    Add functionality to get product list from view model and ui update

--- app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductListFragment.kt
+++ app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductListFragment.kt
@@ -1,12 +1,26 @@
 package com.example.dubizzletest.presentation.productlist
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dubizzletest.R
 import com.example.dubizzletest.base.BaseFragment
+import com.example.dubizzletest.customview.ProgressDialogFragment
+import com.example.dubizzletest.domain.entities.Product
+import com.example.dubizzletest.presentation.util.ImageCache
+import com.example.dubizzletest.presentation.util.ResultObserver
+import com.example.dubizzletest.presentation.util.toast
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
 
 /**
  * Main UI for Product list.
@@ -15,6 +29,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class ProductListFragment : BaseFragment() {
 
     private lateinit var fragmentView: View
+    private val productViewModel: ProductViewModel by activityViewModels()
+    private lateinit var progressDialogFragment: ProgressDialogFragment
+
+    @Inject
+    lateinit var imageCache: ImageCache
+
+    @Inject
+    lateinit var productListAdapter: ProductRecyclerAdapter
+    lateinit var rvProduct: RecyclerView
 
     companion object {
         fun newInstance() = ProductListFragment()
@@ -36,10 +59,60 @@ class ProductListFragment : BaseFragment() {
     }
 
     private fun setupViews() {
-
+        rvProduct = fragmentView.findViewById(R.id.rv_product)
+        rvProduct.adapter = productListAdapter
+        rvProduct.layoutManager = GridLayoutManager(
+            this.context,
+            2
+        )
+        rvProduct.addItemDecoration(
+            DividerItemDecoration(
+                rvProduct.context,
+                DividerItemDecoration.HORIZONTAL
+            )
+        )
+        rvProduct.addItemDecoration(
+            DividerItemDecoration(
+                rvProduct.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        progressDialogFragment = ProgressDialogFragment.newInstance()
     }
 
     private fun observeDataChanges() {
+        productViewModel.productList.observe(
+            viewLifecycleOwner,
+            ResultObserver(
+                hideLoading = ::hideLoading,
+                showLoading = ::showLoading,
+                onSuccess = ::updateProductList,
+                onError = ::showErrorMessage
+            )
+        )
+    }
 
+    private fun hideLoading() {
+        progressDialogFragment.dismiss()
+    }
+
+    private fun showLoading() {
+        activity?.supportFragmentManager?.let { fragmentManager ->
+            progressDialogFragment.show(fragmentManager, null)
+        }
+    }
+
+    /**
+     * Update recycler view
+     */
+    private fun updateProductList(productList: List<Product>) {
+        productListAdapter.setProductList(productList)
+    }
+
+    /**
+     * Show toast message if Exception is thrown.
+     */
+    private fun showErrorMessage(message: String) {
+        this.activity?.toast(message)
     }
 }
--- app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductViewModel.kt
+++ app/src/main/java/com/example/dubizzletest/presentation/productlist/ProductViewModel.kt
@@ -7,9 +7,10 @@ import androidx.lifecycle.viewModelScope
 import com.example.dubizzletest.domain.common.Result
 import com.example.dubizzletest.domain.entities.Product
 import com.example.dubizzletest.domain.usecases.GetProductsUseCase
+import com.example.dubizzletest.presentation.util.ImageCache
+import com.example.dubizzletest.presentation.util.getImageBitmap
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,10 +22,32 @@ class ProductViewModel @Inject constructor(
 
     val productList: LiveData<Result<List<Product>>> = _productList
 
+    @Inject
+    lateinit var imageCache: ImageCache
+
     init {
         _productList.value = Result.Loading
         viewModelScope.launch(Dispatchers.IO) {
             val repositoryResult = getProductsUseCase.getProductList()
+            val imageDownloadJobs = ArrayList<Job>()
+            if (repositoryResult is Result.Success) {
+                supervisorScope {
+                    for (product in repositoryResult.data) {
+                        if (product.images != null) {
+                            for (imageUrl in product.images) {
+                                val job = launch(Dispatchers.IO) {
+                                    imageCache.addImageToCache(
+                                        imageUrl,
+                                        getImageBitmap(imageCache, imageUrl)
+                                    )
+                                }
+                                imageDownloadJobs.add(job)
+                            }
+                        }
+                    }
+                }
+            }
+            imageDownloadJobs.joinAll()
             _productList.postValue(repositoryResult)
         }
     }
