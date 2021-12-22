b84317c1806c55603f100d5f46d9273977873eed
mayamohite <mayapmohite@gmail.com>
2021-12-22 13:11:57 +0530

    Add logger function in ui util

--- app/src/main/java/com/example/dubizzletest/presentation/util/Util.kt
+++ app/src/main/java/com/example/dubizzletest/presentation/util/Util.kt
@@ -1,11 +1,22 @@
 package com.example.dubizzletest.presentation.util
 
 import android.content.Context
+import android.os.Build
 import android.widget.Toast
+import com.example.dubizzletest.BuildConfig
 
 /**
  * Function to show toast
  */
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * Log messages in debug mode
+ */
+fun log(tag: String, message: String) {
+    if (BuildConfig.DEBUG) {
+        println("$tag : $message")
+    }
 }
