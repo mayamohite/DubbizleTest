635219356343b6bb705742a8ec1b4cfb9ea8d265
mayamohite <mayapmohite@gmail.com>
2021-12-23 08:07:59 +0530

    Add espresso idling in image download class

--- app/src/main/java/com/example/dubizzletest/presentation/util/ImageDownloadUtil.kt
+++ app/src/main/java/com/example/dubizzletest/presentation/util/ImageDownloadUtil.kt
@@ -8,23 +8,25 @@ import java.net.URL
 const val TAG = "Image Download"
 
 fun getImageBitmap(imageCache: ImageCache, imageUrl: String): Bitmap? {
-    var image: Bitmap? = null
-    var inSampleSize = 0
-    if (imageCache.getImageFromCache(imageUrl) == null) {
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        options.inSampleSize = inSampleSize
-        try {
-            val url = URL(imageUrl)
-            val connection = url.openConnection() as HttpURLConnection
-            val stream = connection.inputStream
-            options.inJustDecodeBounds = false
-            image = BitmapFactory.decodeStream(stream, null, options)
-            log(TAG, "Download success")
-            return image
-        } catch (e: Exception) {
-            log(TAG, "Exception - $e")
+    wrapEspressoIdlingResource {
+        var image: Bitmap? = null
+        var inSampleSize = 0
+        if (imageCache.getImageFromCache(imageUrl) == null) {
+            val options = BitmapFactory.Options()
+            options.inJustDecodeBounds = true
+            options.inSampleSize = inSampleSize
+            try {
+                val url = URL(imageUrl)
+                val connection = url.openConnection() as HttpURLConnection
+                val stream = connection.inputStream
+                options.inJustDecodeBounds = false
+                image = BitmapFactory.decodeStream(stream, null, options)
+                log(TAG, "Download success")
+                return image
+            } catch (e: Exception) {
+                log(TAG, "Exception - $e")
+            }
         }
+        return image
     }
-    return image
 }
